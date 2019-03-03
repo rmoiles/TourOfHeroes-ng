098d2072c81f87de602584bc7a815b8c8c7fa55f
Robert Moiles <robert.moiles@gmail.com>
2019-03-03 15:46:26 -0600

    routing

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../domain/hero';
+import { HeroService } from '../services/hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
+})
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => {
+        return this.heroes = heroes.slice(0, 5);
+      });
+  }
+
+}
